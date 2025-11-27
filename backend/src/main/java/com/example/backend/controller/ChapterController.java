f50a2b6e1eb2233f7e00cca5202c3d414bfedaf9
HMtsumura <hironori0811@gmail.com>
2025-11-27 23:15:54 +0900

    moved getChaptersByBookId to ChapterCpntroller

--- backend/src/main/java/com/example/backend/controller/ChapterController.java
+++ backend/src/main/java/com/example/backend/controller/ChapterController.java
@@ -1,7 +1,9 @@
 package com.example.backend.controller;
 
 import com.example.backend.entity.Book;
+import com.example.backend.entity.Chapter;
 import com.example.backend.repository.BookRepository;
+import com.example.backend.repository.ChapterRepository;
 import com.example.backend.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,27 +12,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
-public class BookController {
-    private final BookService bookService;
+public class ChapterController {
 
     @Autowired
-    private BookRepository bookRepository;
+    private ChapterRepository chapterRepository;
 
-    public BookController(BookService bookService) {
-        this.bookService = bookService;
-    }
-
-    // IDで参考書取得
-    @GetMapping("/{id}")
-    public ResponseEntity<Book> getBookById(@PathVariable Integer id) {
-
-        Optional<Book> bookOpt = bookRepository.findById(id);
-
-        // 参考書が存在しない場合は 404
-        return bookOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    @GetMapping("/{bookId}/chapters")
+    public List<Chapter> getChaptersByBookId(@PathVariable Integer bookId) {
+        return chapterRepository.findByBookId(bookId);
     }
 }
