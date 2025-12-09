082942950e9ad007598f18d54af6e63ad8bf4e76
HMtsumura <hironori0811@gmail.com>
2025-12-09 23:20:04 +0900

    added webMVCConfig

---
+++ backend/src/main/java/com/example/backend/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/uploads/**")           // ブラウザから呼ぶパス
+                .addResourceLocations("file:uploads/");      // サーバー上の保存先
+    }
+}
+
